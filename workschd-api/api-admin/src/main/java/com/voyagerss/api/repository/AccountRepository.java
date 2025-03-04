bde8b2007dfd8526dae17145ed0a0702d32e2b3d
minssan9 <minssan9@gmail.com>
2025-03-04 20:36:06 +0900

    feat/22-login) get Account orElse get AccountSns

--- workschd-api/api-admin/src/main/java/com/voyagerss/api/repository/AccountRepository.java
+++ workschd-api/api-admin/src/main/java/com/voyagerss/api/repository/AccountRepository.java
@@ -1,1 +1,13 @@
- 
+package com.voyagerss.api.repository;
+
+import com.voyagerss.persist.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByEmail(String email);
+} 
