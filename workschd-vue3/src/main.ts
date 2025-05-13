db2dfd28e70975360f3a778087abbe0f00f50b7b
sanghun.min <sanghun.min@hlcompany.com>
2025-05-13 20:55:42 +0900

    master) firebase disabled

--- workschd-vue3/src/main.ts
+++ workschd-vue3/src/main.ts
@@ -8,7 +8,7 @@ import { Quasar, Notify } from 'quasar'
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
 import { i18n } from "@/locales/i18n";
-import { app as firebaseApp } from './firebase/config'  // Import Firebase
+// import { app as firebaseApp } from './firebase/config'  // Import Firebase
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -32,7 +32,7 @@ app.use(Quasar, {
 
 
 // Make Firebase available throughout the app
-app.config.globalProperties.$firebase = firebaseApp 
+// app.config.globalProperties.$firebase = firebaseApp 
 
 declare global {
   interface Window {
